442309b54113996753f61e6a39203cf4539d0186
Kais Salha <kaiss.salha@gmail.com>
2024-05-26 12:27:23 -0400

    redirect to logout on 401

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -8,6 +8,7 @@ import {
   ApolloProvider,
   InMemoryCache,
   createHttpLink,
+  from,
 } from "@apollo/client";
 import { Provider } from "jotai";
 import { jotaiStore } from "./lib/jotai-store";
@@ -15,14 +16,25 @@ import { ThemeProvider } from "next-themes";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { persistOptions, queryClientOptions } from "@/lib/query-client";
 import { QueryClient } from "@tanstack/react-query";
+import { onError } from "@apollo/client/link/error";
 
 const httpLink = createHttpLink({
   uri: `${import.meta.env.VITE_API_ENDPOINT}/graphql`,
   credentials: "include",
 });
 
+const errorLink = onError(({ operation }) => {
+  const { response } = operation.getContext();
+  if (response.status === 401) {
+    client.clearStore();
+    router.navigate({
+      to: "/login",
+    });
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
